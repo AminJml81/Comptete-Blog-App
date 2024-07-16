8a5bb37ca50efea7be2de403fa42181b6e4da8c0
Amin <Aminbest1381@gmail.com>
2024-07-16 22:46:07 +0330

    added next, prev post

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from blog.models import Post
+from django.utils import timezone
 from django.core.paginator import Paginator
 from django.db.models import Q
 
 # Create your views here.
 def index_view(request,*args, **kwargs):
-    posts = Post.objects.filter(status=True)
+    posts = Post.objects.filter(published_date__lte=timezone.now(), status=True)
 
     if (category :=request.GET.get('category')):
         posts = posts.filter(categories__name=category.title())
@@ -28,12 +29,23 @@ def index_view(request,*args, **kwargs):
     return render(request, 'index.html', context)
 
 def blog_single_view(request, pid:int):
-    post = Post.objects.filter(id=pid, status=True).first()
+    post = Post.objects.filter(id=pid, status=True, published_date__lte=timezone.now()).first()
+    context = {}
     if post:
-        increment_post(post)
-        context = {'post':post}
-        return render(request, 'blog-single.html', context)
+        prev_post, next_post = find_prev_next_posts(post)
+        increment_post_view(post)
+        context = {'prev_post':prev_post, 'post':post, 'next_post':next_post}
 
-def increment_post(post: Post):
+    return render(request, 'blog-single.html', context)
+
+def find_prev_next_posts(post: Post):
+    posts = list(Post.objects.filter(published_date__lte=timezone.now(), status=1))
+    main_post_index = posts.index(post)
+    prev_post = None if main_post_index == 0 else posts[main_post_index-1]
+    next_post = None if len(posts) == 1 else posts[main_post_index + 1]
+    print(next_post)
+    return prev_post, next_post
+
+def increment_post_view(post: Post):
     post.views += 1
     post.save()
